883b59559de22d153cdfde3f563acbe032405b8a
Ptz_89 <ptz_89@mail.ru>
2021-11-19 16:37:16 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Battery {
+    int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+    }
+
+}
