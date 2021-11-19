813b338ac64ed469e1f4834cc30717e9ade1e62c
Ptz_89 <ptz_89@mail.ru>
2021-11-19 10:47:33 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активное " + active);
+        System.out.println("Статус " + status);
+        System.out.println("Сообщение " + message);
+    }
+
+    public static void main(String[] args) {
+        Error er = new Error();
+        Error er1 = new Error(true, 10, "String");
+        Error er2 = new Error(false, 0, "zero");
+        er.printInfo();
+        er1.printInfo();
+        er2.printInfo();
+    }
+}
