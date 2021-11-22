12cc5947a49a46fb3a7fbdf77e9f1eb1d5e01cc7
Ptz_89 <ptz_89@mail.ru>
2021-11-22 18:11:03 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,16 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+
+    }
 
     public Item(String name) {
 
@@ -30,8 +38,8 @@ public class Item {
         this.name = name;
     }
 
-    public Item() {
-
+    public LocalDateTime getCreated() {
+        return created;
     }
-
 }
+
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(new Item().getCreated().format(formatter));
+    }
+}
