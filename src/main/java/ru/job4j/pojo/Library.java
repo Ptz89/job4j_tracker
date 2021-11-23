2d15b7a98c835a8aea1e41b17d0b8128991916eb
Ptz_89 <ptz_89@mail.ru>
2021-11-23 13:36:25 +0300

    3. Массивы и модели. [#174892 #231894]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,29 +2,24 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book book1 = new Book("Clean code", 1);
-        Book book2 = new Book("100 idea", 100);
-        Book book3 = new Book("Three rules", 3);
-        Book book4 = new Book("Dictionary", 52);
         Book[] books = new Book[4];
-        books[0] = book1;
-        books[1] = book2;
-        books[2] = book3;
-        books[3] = book4;
-        for (Book bk : books) {
-            System.out.println(bk.getName() + " - " + bk.getCount());
-        }
-        Book rst = books[0];
+        books[0] = new Book("Clean code", 1);
+        books[1] = new Book("100 idea", 100);
+        books[2] = new Book("Three rules", 3);
+        books[3] = new Book("Dictionary", 52);
+        printBook(books, null);
+        Book temp = books[0];
         books[0] = books[3];
-        books[3] = rst;
-        for (Book bk : books) {
-            System.out.println(bk.getName() + " - " + bk.getCount());
-        }
-        for (Book bk : books) {
-            if (bk.getName().equals("Clean code")) {
-                System.out.println(bk.getName() + " - " + bk.getCount());
+        books[3] = temp;
+        printBook(books, null);
+        printBook(books, "Clean code");
+    }
+
+    private static void printBook(Book[] books, String bookName) {
+        for (Book book : books) {
+            if (bookName == null || book.getName().equals(bookName)) {
+                System.out.println(book.getName() + " - " + book.getCount());
             }
         }
-
     }
 }
