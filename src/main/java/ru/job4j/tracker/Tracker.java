20b0344849f9f83d4f03dd911250bf831bd7961b
Ptz_89 <ptz_89@mail.ru>
2021-11-26 20:19:32 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #233016]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -38,6 +38,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
+
             items[indexOf(id)].setName(item.getName());
         }
         return rsl;
