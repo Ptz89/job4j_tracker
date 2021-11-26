07b9a7ddf85ea29f78ec8c1d1e3e138eddbb109a
Ptz_89 <ptz_89@mail.ru>
2021-11-26 12:16:01 +0300

    3. Реализация профессий в коде [#6837 #231504]

--- src/main/java/ru/job4j/inheritance/Builder.java
+++ src/main/java/ru/job4j/inheritance/Builder.java
@@ -3,7 +3,12 @@ package ru.job4j.inheritance;
 public class Builder extends Engineer {
     private String type;
 
-    public Builder(String name, String surname, String education, String birthday) {
-        super(name, surname, education, birthday);
+    public Builder(String name, String surname, String education, String birthday, int experience, String type) {
+        super(name, surname, education, birthday, experience);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
     }
 }
--- src/main/java/ru/job4j/inheritance/Surgeon.java
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -3,8 +3,9 @@ package ru.job4j.inheritance;
 public class Surgeon extends Doctor {
     private String kind;
 
-    public Surgeon(String name, String surname, String education, String birthday) {
-        super(name, surname, education, birthday);
+    public Surgeon(String name, String surname, String education, String birthday, String clinic, String kind) {
+        super(name, surname, education, birthday, clinic);
+        this.kind = kind;
     }
 
     public String getKind() {
