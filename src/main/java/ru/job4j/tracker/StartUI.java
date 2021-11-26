1b06180210f03f0b90a31075f97b10d9f83d412d
Ptz_89 <ptz_89@mail.ru>
2021-11-26 14:49:47 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,6 @@ public class StartUI {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(new Item().getCreated().format(formatter));
+        System.out.println(new Item(3, "Dima"));
     }
 }
