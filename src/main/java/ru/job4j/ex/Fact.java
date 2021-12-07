a87b1623f7f731e45959b252776b8c8b6e290e4e
Ptz_89 <ptz_89@mail.ru>
2021-12-07 15:31:01 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #236537]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,7 +2,7 @@ package ru.job4j.ex;
 
 public class Fact {
     public static void main(String[] args) {
-        new Fact().calc(2);
+        System.out.println(new Fact().calc(-1));
     }
 
     public static int calc(int n) {
@@ -10,9 +10,9 @@ public class Fact {
             throw new IllegalArgumentException("Enter a positive number");
         }
         int rsl = 1;
-            for (int index = 1; index <= n; index++) {
+        for (int index = 1; index <= n; index++) {
                 rsl *= index;
-            }
+        }
         return rsl;
     }
 }
