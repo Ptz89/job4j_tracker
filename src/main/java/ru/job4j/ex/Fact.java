dfa7edd40c73aab84b05c1104aca86e3679168eb
Ptz_89 <ptz_89@mail.ru>
2021-12-08 11:18:07 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #236537]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,15 +2,15 @@ package ru.job4j.ex;
 
 public class Fact {
     public static void main(String[] args) {
-        System.out.println(new Fact().calc(-1));
+        System.out.println(new Fact().calc(0));
     }
 
     public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("Enter a positive number");
+            throw new IllegalArgumentException("Factorials of negative integers are undefined");
         }
         int rsl = 1;
-        for (int index = 1; index <= n; index++) {
+        for (int index = 2; index <= n; index++) {
                 rsl *= index;
         }
         return rsl;
