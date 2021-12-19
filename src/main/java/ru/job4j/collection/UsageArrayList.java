0c0299cfefcb2fba0aa3700f1ca1d8405f89387c
Ptz_89 <ptz_89@mail.ru>
2021-12-19 12:00:23 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> array = new ArrayList<>();
+        array.add("Petr");
+        array.add("Ivan");
+        array.add("Stepan");
+        for (String str : array) {
+            System.out.println(str);
+        }
+    }
+}
