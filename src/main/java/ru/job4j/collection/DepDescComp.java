0de509e09786e3240f4a23ad6121aa4980dcf200
Ptz_89 <ptz_89@mail.ru>
2022-01-20 11:26:41 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        String[] strArr1 = o1.split("/", 2);
+        String[] strArr2 = o2.split("/", 2);
+        int rsl = strArr2[0].compareTo(strArr1[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
+    }
+}
---
+++ src/main/java/ru/job4j/collection/Test.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+
+public class Test {
+    private static String slash = "/";
+
+    public static void main(String[] args) {
+        String str1 = "K1/SK1/SSK1";
+        String str2 = "K2/SK1/SSK1";
+        System.out.println(str1.compareTo(str2));
+    }
+}
