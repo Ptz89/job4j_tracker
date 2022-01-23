e2e91cba7c109a6fb55fa1852ca8b6d0b046b336
Ptz_89 <ptz_89@mail.ru>
2022-01-23 18:56:17 +0300

    2. Отсортировать департаменты [#34352 #253322]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -8,9 +8,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                String dep = start + (start.isEmpty() ? "" : "/") + el;
-                tmp.add(dep);
-                start = dep;
+                start += start.isEmpty() ? el : "/" + el;
+                tmp.add(start);
             }
         }
         return new ArrayList<>(tmp);
